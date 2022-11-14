e6f926bca350645e5b3a07370b6b96eede5e876d
vipf823 <vipf823@DESKTOP-3HL79NN>
2022-11-14 21:52:51 +0900

    day02- MainClass04~07

---
+++ src/day02/test/MainClass07.java
@@ -0,0 +1,23 @@
+package day02.test;
+/*
+ *  [[참조형 데이터형(레퍼런스형) ]]
+ *  
+ *  5. string 형 (문자열)
+ *   - 선언하는 방법
+ *       String 변수명;
+ *   -문자열을 저장할 수 있다.
+ *   
+ *   
+ * 기본형:boolean, char, byte , short , int , long , float , double
+ *   
+ */
+
+public class MainClass07 {
+    public static void main(String[] args) {
+    	//변수 선언과 동시에 값 대입하기
+    	String str = "안녕하세요.";
+    	System.out.println("str : " + str);
+	
+}
+
+}
